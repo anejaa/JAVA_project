f0c16c9269005df4b9812c2d0c8cfa8bf5ef819f
aneja <aneja.pasalic@gmail.com>
2024-03-24 16:52:30 +0100

    Dodala ločene menije za uporabnike, vozičke in produkte, ter metode za iskanje posameznega uporabnika, produkta, vozička.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,13 +13,83 @@ public class Main {
 
             switch (option) {
                 case 1:
-                    displayUsers();
+                    int userOption;
+                    do {
+                        displayUserOptions();
+                        userOption = scanner.nextInt();
+
+                        switch (userOption) {
+                            case 1:
+                                displayUsers();
+                                break;
+                            case 2:
+                                System.out.print("Enter user id: ");
+                                int id = scanner.nextInt();
+                                showUser(id);
+                                break;
+                            case 3:
+
+                                break;
+                            case 4:
+                                System.out.println("Returning to main menu...");
+                                break;
+                            default:
+                                System.out.println("Invalid option, please try again.");
+                                break;
+                        }
+                    } while (userOption != 4);
                     break;
                 case 2:
-                    displayCarts();
+                    do {
+                        displayCartOptions();
+                        userOption = scanner.nextInt();
+
+                        switch (userOption) {
+                            case 1:
+                                displayCarts();
+                                break;
+                            case 2:
+                                System.out.print("Enter cart id: ");
+                                int id = scanner.nextInt();
+                                showCart(id);
+                                break;
+                            case 3:
+
+                                break;
+                            case 4:
+                                System.out.println("Returning to main menu...");
+                                break;
+                            default:
+                                System.out.println("Invalid option, please try again.");
+                                break;
+                        }
+                    } while (userOption != 4);
                     break;
                 case 3:
-                    displayProducts();
+                    do {
+                        displayProductOptions();
+                        userOption = scanner.nextInt();
+
+                        switch (userOption) {
+                            case 1:
+                                displayProducts();
+                                break;
+                            case 2:
+                                System.out.print("Enter product id: ");
+                                int id = scanner.nextInt();
+                                showProduct(id);
+                                break;
+                            case 3:
+
+                                break;
+                            case 4:
+                                System.out.println("Returning to main menu...");
+                                break;
+                            default:
+                                System.out.println("Invalid option, please try again.");
+                                break;
+                        }
+                    } while (userOption != 4);
                     break;
                 case 4:
                     System.out.println("Finishing program.");
@@ -29,6 +99,66 @@ public class Main {
             }
         } while (option != 4);
         scanner.close();
+    }
+
+    private static void showProduct(int id) {
+        String uri = "https://dummyjson.com/products/" + id;
+        try {
+            String res = ApiClient.doGetRequest(uri);
+            System.out.println(res);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayProductOptions() {
+        System.out.println("Select Menu");
+        System.out.println("1. All products");
+        System.out.println("2. Find product");
+        System.out.println("3. Edit product");
+        System.out.println("3. Delete product");
+        System.out.println("4. Go back");
+        System.out.print("Select option: ");
+    }
+
+    private static void showCart(int id) {
+        String uri = "https://dummyjson.com/carts/" + id;
+        try {
+            String res = ApiClient.doGetRequest(uri);
+            System.out.println(res);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayCartOptions() {
+        System.out.println("Select Menu");
+        System.out.println("1. All carts");
+        System.out.println("2. Find cart ");
+        System.out.println("3. Edit cart");
+        System.out.println("3. Delete cart");
+        System.out.println("4. Go back");
+        System.out.print("Select option: ");
+    }
+
+    private static void showUser(int id) {
+        String uri = "https://dummyjson.com/users/" + id;
+        try {
+            String res = ApiClient.doGetRequest(uri);
+            System.out.println(res);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayUserOptions() {
+        System.out.println("Select Menu");
+        System.out.println("1. All users");
+        System.out.println("2. Find user ");
+        System.out.println("3. Edit user");
+        System.out.println("3. Delete user");
+        System.out.println("4. Go back");
+        System.out.print("Select option: ");
     }
 
     private static void displayProducts() {
